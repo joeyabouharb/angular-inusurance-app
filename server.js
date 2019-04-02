109fcdd9da3a4e5c4d0b53c29ece4b6839932010
joey abouharb <joey.abouharb@gmail.com>
2019-04-02 16:27:43 +1100

    fix deployment

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors')
 app.use(express.static(__dirname + '/dist/client'));
+
+app.use(cors());
 
 app.listen(process.env.PORT || 6080, () => {
   console.log(`listening at 6080`)
 });
 
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './dist/client/index.html'));
 });
+
+
